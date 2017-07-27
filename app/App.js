a5a47d3d413985471aacbb8e2c0af2fc18dd83e2
Silvio Custodio <it.silviocustodio@gmail.com>
2017-07-26 21:46:32 -0300

    First commit - add files

---
+++ app/App.js
@@ -0,0 +1,11 @@
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var GitHub = require('./components/GitHub');
+
+
+
+ReactDOM.render(<GitHub />, document.getElementById('app'));
+
+
